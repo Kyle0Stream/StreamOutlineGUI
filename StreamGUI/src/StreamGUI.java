6199aa698bb71559710118d85a124463b33df551
Forrest Stonedahl <forrest@stonedahl.com>
2013-07-01 13:39:43 -0500

    added comment about overall plan

--- StreamGUI/src/StreamGUI.java
+++ StreamGUI/src/StreamGUI.java
@@ -1,4 +1,28 @@
 
+/* Notes by Forrest (7/1/2013)
+ * 
+ *  Things to get from the image (e.g. DSCN001.JPG or DSCN001.PNG) itself:  
+ *      - image width, image height
+ *      - locations of the four corners of the square (in pixels)
+ * 
+ * Things to get from the user:
+ *      - Which corners are which: label ABCD the red(?) corners of the square
+ *      - Horizontal Field of View (HFOV) of the camera that took the pictures.
+ *        (we think our red/silver cameras are either 67.5 or 69.5 or something,
+ *         but we need our software to work for all cameras).
+ * 
+ *      - Real-world length of each side of the square (e.g. 8)
+ * 
+ * Then, either:
+ *      A) create a text file (e.g. DSCN001.input.txt) with all the info.
+ *         and run the MatLab program that will do the actual conversion
+ *   or B) we should translate the MatLab program into Java, and actually do
+ *        the math needed to "rectify" the perspective of the image
+ * 
+ * Eventually, we may want a program (probably a separate program?) for merging
+ * multiple rectified images. We'll deal with that later.
+ */
+
 /**
  *
  * @author kl0601084
