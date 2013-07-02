65d03a10fbc1c298ef3cc623ee1a207cc87f63e7
Kyle <Kyle@Kyle-PC>
2013-07-02 18:09:07 -0500

    Added counters for each color.

--- Pixel/src/FindPixel.java
+++ Pixel/src/FindPixel.java
@@ -7,16 +7,16 @@ import javax.imageio.ImageIO;
 
 public class FindPixel
 {
-    public static int yellowCount = 1;
-    public static int magentaCount = 1;
-    public static int redCount = 1;
+    public static int yellowCount = 0;
+    public static int magentaCount = 0;
+    public static int redCount = 0;
     public final static String IMG = "dscn0152nobackground.png";
     public static void main(String[] args) throws IOException 
     {
         //read image
         BufferedImage picImage = ImageIO.read(FindPixel.class.getResource(IMG));
         //write file
-        FileWriter log = new FileWriter("E:/JAVA/Pixel/src/log.txt");
+        FileWriter log = new FileWriter("E:/JAVA/StreamOutlineGUI/Pixel/src/log.txt");
         BufferedWriter out = new BufferedWriter(log);
 
         for(int y = 0; y < picImage.getHeight(); y++)
@@ -27,26 +27,29 @@ public class FindPixel
                 
                 if (c.getRed() == 255 && c.getGreen() == 255 && c.getBlue() == 0)
                 {
+                    yellowCount++;
                     out.write(yellowCount + ") " + "Yellow Pixel found at = " + x + "," + y);
                     out.newLine();
-                    yellowCount++;
                 }
                 if (c.getRed() == 255 && c.getGreen() == 0 && c.getBlue() == 255)
                 {
-                    //System.out.println("Happened");
+                    magentaCount++;
                     out.write(magentaCount + ") " + "Magenta Pixel found at = " + x + "," + y);
                     out.newLine();
-                    magentaCount++;
                 }
                 if (c.getRed() == 255 && c.getGreen() == 0 && c.getBlue() == 0)
                 {
-                    //System.out.println("Happened");
+                    redCount++;
                     out.write(redCount + ") " + "Red Pixel found at = " + x + "," + y);
                     out.newLine();
-                    redCount++;
                 }
             }
         }
+        out.write("Total Amount of Red pixels: " + redCount);
+        out.newLine();
+        out.write("Total Amount of Magenta pixels: " + magentaCount);
+        out.newLine();
+        out.write("Total Amount of Yellow pixels: " + yellowCount);
         out.close();
     }  
 }
