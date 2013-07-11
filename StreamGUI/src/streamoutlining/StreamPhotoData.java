82e17d06ea1f475089de2bda9ead66791cc366f4
Forrest Stonedahl <forrest@stonedahl.com>
2013-07-11 10:56:12 -0500

    starting work on separating data from GUI

---
+++ StreamGUI/src/streamoutlining/StreamPhotoData.java
@@ -0,0 +1,20 @@
+package streamoutlining;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class StreamPhotoData {
+    
+    private File originalJPEGFile;
+    private BufferedImage originalJPEGImage;
+    private BufferedImage outlinePNGImage;
+    private BufferedImage pointsPNGImage;
+    
+    public StreamPhotoData(File originalJPEGFile)
+    {
+        
+        
+    }
+    
+    
+}
